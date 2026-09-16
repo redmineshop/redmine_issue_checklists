b0813ea6df0e453717f22e2423d5f461c81524d5
Tuan Nguyen <tuan.m4u@gmail.com>
2026-09-16 22:37:12 +0700

    Initial public release of redmine_issue_checklists

---
+++ lib/redmine_issue_checklists/version.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Zeitwerk maps this file to RedmineIssueChecklists::Version.
+module RedmineIssueChecklists
+  module Version
+    STRING = '1.0.0'
+  end
+
+  VERSION = Version::STRING
+end
