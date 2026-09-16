8caab1a411e87c5b9318091c2c5395c6662019e9
Tuan Nguyen <tuan.m4u@gmail.com>
2026-09-16 22:37:10 +0700

    Initial public release of redmine_issue_checklists

---
+++ init.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'redmine'
+
+require_relative 'lib/redmine_issue_checklists/version'
+require_relative 'lib/redmine_issue_checklists/hooks'
+require_relative 'lib/redmine_issue_checklists/issue_patch'
+
+Redmine::Plugin.register :redmine_issue_checklists do
+  name 'Redmine Issue Checklists'
+  author 'RedmineShop'
+  author_url 'https://redmineshop.com'
+  description 'Interactive checklists on Redmine issues — add items, toggle done, track progress. Community edition, free forever.'
+  version RedmineIssueChecklists::VERSION
+  url 'https://redmineshop.com/products/redmine-issue-checklists'
+
+  requires_redmine version_or_higher: '5.0'
+
+  project_module :issue_tracking do
+    permission :manage_issue_checklists,
+               { issue_checklists: %i[create destroy toggle] }
+  end
+end
+
+module RedmineIssueChecklists
+  def self.patch_issue!
+    return if Issue.included_modules.include?(IssuePatch)
+
+    Issue.include IssuePatch
+  end
+end
+
+RedmineIssueChecklists.patch_issue!
+
+reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Callbacks
+reloader.to_prepare do
+  RedmineIssueChecklists.patch_issue!
+end
